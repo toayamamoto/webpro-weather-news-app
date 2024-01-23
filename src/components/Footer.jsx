db2cd92f9f504faf1aed0dab3741f1c4ae858c62
Toa <chtu21015@g.nihon-u.ac.jp>
2024-01-23 20:02:50 +0900

    修正

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer" style={{ display: 'flex', justifyContent: 'center' }}>
       <div className="content has-text-centered">
-        <p>ニュースと天気は下記のAPIから入手しました</p>
+        <p>ニュースと天気とポケモンは下記のAPIから入手しました</p>
         <p>
           <a href="https://newsapi.org/" target="_blank" rel="noopener noreferrer">
             News API
@@ -13,6 +13,9 @@ export default function Footer() {
             OpenWeatherMap API
           </a>
         </p>
+        <a href="https://pokeapi.co/" target="_blank" rel="noopener noreferrer">
+        Pokémon API
+          </a>
       </div>
     </footer>
   );
